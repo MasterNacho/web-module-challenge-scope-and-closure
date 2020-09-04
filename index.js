95d3ebb7ac55afd0f118b422ad20012dfac4da38
Carlos McConnell <deseanmcconnell@gmail.com>
2020-09-04 19:03:28 -0400

    First Push

--- index.js
+++ index.js
@@ -28,9 +28,15 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * Counter1 is returning another function within itself, where as Counter2 is returing the same function. 
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * The Counter1 because it has another function inside of it.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
+ * It would allow for room for more dynamic code. Counter2 would require one to hard code everything. 
  *
 */
 
@@ -56,12 +62,14 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
 
-    /*Code Here*/
+    return Math.floor(Math.random()*3)
 
-}
+} 
 
+console.log(inning());
+console.log(inning());
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
@@ -76,11 +84,14 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(myFun, Innings){
 
-  /*Code Here*/
+  let finalScore = {
+    Home: 0;,
+    Away: 0;
+  };
 
-}
+ 
 
 /* Task 4: 
 
